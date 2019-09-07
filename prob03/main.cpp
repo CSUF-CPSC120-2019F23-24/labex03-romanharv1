11ac5f2384b823335d39ba0171ff2ee2cfc1fc19
romanharv1 <romanharv1@csu.fullerton.edu>
2019-09-07 14:43:59 -0700

    Created inches to feet and inches converter.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+
+  // Defining variables.
+  int height, a_foot, feet, inches;
+  a_foot = 12;
+
+  // Getting height in inches.
+  std::cout << "Please enter the person's height in inches: ";
+  std::cin >> height;
+
+  // Converting height to feet and inches.
+  feet = height / 12;
+  inches = height % 12;
+  std::cout << "That person is " << feet << "'" << inches << "\"\n";
+
+
+  return 0;
+}

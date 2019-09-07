5da0f05cb9f4bbcdfe40ab8d2e6e828ad4c2a760
romanharv1 <romanharv1@cs104u-5a.acad.fullerton.edu>
2019-09-07 13:36:52 -0700

    completed to-do's

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -6,9 +6,9 @@ int main()
 {
   // TODO#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 107;   // decimal
+  char my_char2 = 0x6B; // hex
+  char my_char3 = 'k';  // character
 
   std::cout << "Char1: " << my_char1 << std::endl;
   std::cout << "Char2: " << my_char2 << std::endl;
@@ -21,24 +21,29 @@ int main()
   // however, use a different method to do so for each cout statement
   // hint: use the ASCII character set
   std::cout << "+" << std::endl;
+  std::cout << '+' << '\n';
+  char my_char = 0x2B;
+  std::cout << my_char << '\n';
+  char my_char4 = 43;
+  std::cout << my_char4 << '\n';
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << "\'Y\'" << '\n';
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#4: try changing the variable types and the data types in the
   // statements below to see what is returned by the sizeof operator
-  bool my_bool = 1;
+  short my_bool = 11;
   std::cout << my_bool << ' ' << sizeof(my_bool) << '\n';
-  short my_int = 1;
+  int my_int = 0x2B;
   std::cout << my_int << ' ' << sizeof(my_int) << '\n';
 
-  double amount;
+  long long amount;
   std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
   std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
 
